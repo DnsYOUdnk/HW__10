3a36147d7c9f405f676484518cab1be8066946a2
Dinis <d.yudenka@yandex.by>
2022-01-20 18:36:16 +0300

    part task

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+
+let NewContact = function () {
+    // this.__proto__ = contact1;
+    ContactSave.apply(this)
+
+    this.showContact = function() {
+        // this.initContact()
+        let elementWrapper = document.querySelector('.app__wrapper'),
+            list = '';
+        console.log(elementWrapper)
+        if(!elementWrapper) return;
+            list += `<div class="app__item">
+                        <p>${this.resultFirstName}</p>
+                        <p>${this.resultPatronymic}</p>
+                        <p>${this.resultLastName}</p>
+                        <p>${this.resultAge}</p>
+                        <p>${this.resultNumber}</p>
+                        <p>${this.resultEmail}</p>
+                    </div>`
+        elementWrapper.innerHTML = `${list}`
+    }
+}
+
+// NewContact.prototype = contact1;
+
+let newContact = new NewContact();
+let newContact2 = new NewContact();
+newContact.getNewContact()
+
+
+let Constructor = function() {
+    
+}
